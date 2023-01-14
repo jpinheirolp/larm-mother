2b60bb28a8890bcf15f669b4ae1e7b1ad7f44c81
João Pinheiro <jpinheirolp@gmail.com>
2023-01-14 21:04:12 +0100

    made move_randomly turn smothly

--- pkg_mother/pkg_mother/move_randomly.py
+++ pkg_mother/pkg_mother/move_randomly.py
@@ -20,24 +20,26 @@ class Move_Randomly(Node):
 
         self.velocity_publisher= self.create_publisher(Twist, '/multi/cmd_nav', 10)
         self.timer = self.create_timer(0.1, self.move_robot) # 0.1 seconds to target a frequency of 10 hertz
-        self.rectangle_closer_y_limit = -0.3
-        self.rectangle_further_away_y_limit = 0.3
-        self.rectangle_left_x_limit = 0
-        self.rectangle_right_x_limit = 0.23
+        self.rectangle_left_y_limit = -0.3
+        self.rectangle_right_y_limit = 0.3
+        self.rectangle_closer_x_limit = 0
+        self.rectangle_further_away_x_limit = 0.23
         self.rectangle_low_z_limit = -0.18
         self.rectangle_high_z_limit = 0.4
-        self.rectangle_center_x_direction = 0
+        self.rectangle_center_y_direction = 0
+
         self.command = "no commmand"
         self.time_interval_rnd_turn = 300 # 0.1s * 300 == 30 seconds
         self.time_to_rnd_turn = 300
-        self.robot_angle = 0
-        self.robot_position_x = 0
-        self.robot_position_y = 0
-        self.robot_position_z = 0
+
+        self.robot_max_angular_speed = 3
+        self.robot_angular_speed = 0
+        self.robot_linear_speed = 0.1
+
         self.mutex_trn_rnd = False
         self.rotation_counter = 0
         
-   
+    
 
     def interpret_obstacles(self,scanMsg):
         print("interpreting ...")
@@ -46,19 +48,26 @@ class Move_Randomly(Node):
         num_points_left = 0
         num_points_right = 0
         tol_of_points_inside_rect = 30
+        sum_for_robot_angular_speed = 0
         for point in obstacles_points:
             point_x_coordinate = point.x
             point_y_coordinate = point.y
             point_z_coordinate = point.z
-            if self.rectangle_closer_y_limit < point_y_coordinate and point_y_coordinate < self.rectangle_further_away_y_limit:
-                if self.rectangle_left_x_limit < point_x_coordinate and point_x_coordinate < self.rectangle_right_x_limit:
+            if self.rectangle_left_y_limit < point_y_coordinate and point_y_coordinate < self.rectangle_right_y_limit:
+                if self.rectangle_closer_x_limit < point_x_coordinate and point_x_coordinate < self.rectangle_further_away_x_limit:
                     if self.rectangle_low_z_limit < point_z_coordinate and point_z_coordinate < self.rectangle_high_z_limit:
 
                         #point is inside rectangle
-                        if point_y_coordinate < self.rectangle_center_x_direction:
+                        if point_y_coordinate < self.rectangle_center_y_direction:
                             num_points_left += 1
+                            # uodating angular speed
+                            left_rect_center = (self.rectangle_left_y_limit - self.rectangle_center_y_direction)/2
+                            sum_for_robot_angular_speed += ((point_y_coordinate - self.rectangle_center_y_direction) * self.robot_max_angular_speed) / right_rect_center
                         else: 
                             num_points_right += 1
+                            right_rect_center = (self.rectangle_right_y_limit - self.rectangle_center_y_direction)/2
+                            sum_for_robot_angular_speed += ((point_y_coordinate - self.rectangle_center_y_direction) * self.robot_max_angular_speed) / right_rect_center
+
         #It`s not gonna work but the idea is good
         if self.time_to_rnd_turn == 0:
             self.command = "turn_to_rnd_position"
@@ -70,9 +79,12 @@ class Move_Randomly(Node):
             if num_points_left < num_points_right:
                 #there is a object on the right
                 self.command = "turn_left"
+                self.robot_angular_speed = abs(sum_for_robot_angular_speed/num_points_left)
             else:
                 #there is a object on the left
                 self.command = "turn_right"
+                self.robot_angular_speed = sum_for_robot_angular_speed/num_points_right
+                
 
     def move_robot(self):
         velo = Twist()
@@ -93,14 +105,14 @@ class Move_Randomly(Node):
             velo.angular.z = 0.1
             velo.angular.x = 0.0
         elif self.command == "go_foward": #would be more optmized with a switch :(
-            velo.linear.x = 0.1
+            velo.linear.x = self.robot_linear_speed
             velo.angular.z = 0.0
         elif self.command == "turn_right":
             velo.linear.x = 0.0
-            velo.angular.z = 0.3 + random()*0.1
+            velo.angular.z = self.robot_angular_speed
         elif self.command == "turn_left":
             velo.linear.x = 0.0
-            velo.angular.z = -0.3 - random()*0.1
+            velo.angular.z = -self.robot_angular_speed
         else:
             return 0
         #print(velo)
